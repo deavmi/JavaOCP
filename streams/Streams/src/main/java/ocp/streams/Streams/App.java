bd1cd3d14bb5f28b2cf72cd70be7a6a166a48f8b
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-16 14:56:20 +0200

    Streams
    - Added parallel streams
    - Added example of reduction
    - Added example of concurrent/parallel grouping

--- streams/Streams/src/main/java/ocp/streams/Streams/App.java
+++ streams/Streams/src/main/java/ocp/streams/Streams/App.java
@@ -3,6 +3,8 @@ package ocp.streams.Streams;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.*;
 
@@ -133,6 +135,88 @@ public class App
     	 * Now let's consume it all
     	 */
     	streamConcatted.forEach(System.out::println);
+    	
+    	System.out.println();
+    	System.out.println("---------------------------------------------------------");
+    	System.out.println();
+    	
+    	/**
+    	 * We can also do parallel streams. These
+    	 * are streams which execute in parallel
+    	 * (on separate threads).
+    	 * 
+    	 * A few threads will process elements from
+    	 * the same stream.
+    	 * 
+    	 * For this reason I need a thread-safe way
+    	 * of accessing a datatype and because 
+    	 * <code>ArrayList</code> is not inherently
+    	 * thread-safe I will have to do this with
+    	 * Collections's <code>synchronizedList</code>.
+    	 */
+    	names = new ArrayList<String>();
+    	names.add("Abby");
+    	names.add("Becky");
+    	names.add("Claire");
+    	names.add("Dickhead");
+    	names.add("Emma");
+    	names.add("Fransisca");
+    	names.add("Gabriella");
+    	names.add("Jess");
+    	names.add("Jane");
+    	List<String> safeNames = Collections.synchronizedList(names);
+    	
+    	/**
+    	 * Let's create our parallel stream now
+    	 */
+    	namesStream = safeNames.parallelStream();
+    	
+    	/**
+    	 * Let's filter these on separate threads
+    	 */
+    	namesStream = namesStream.filter((name) -> (name.length() == 4 || name.length() == 5));
+    	
+    	/**
+    	 * You can see the different threads race to
+    	 * do the underlying system call to write to
+    	 * the standard output and the race to this
+    	 */
+    	namesStream.forEach(System.out::println);
+    	
+    	
+    	
+    	/**
+    	 * Let's do this again but use a collector as a way
+    	 * of collecting all the results of the stream
+    	 */
+    	namesStream = safeNames.parallelStream()
+    						   .filter((name) ->
+    						   					{
+    						   						return name.length() == 4 ||
+    						   							   name.length() == 5;
+    						   					});
+    	
+    	/**
+    	 * We use a <code>Collector</code> as a way
+    	 * to, well, collect the items produced
+    	 * exhaustively by the stream
+    	 * 
+    	 * Now the combination technique used seems
+    	 * to order them for the final list hence
+    	 * we won't see any parallel artifacts
+    	 */
+    	List<String> collected = namesStream.collect(Collectors.toList());
+    	System.out.println(collected);
+    	
+    	/**
+    	 * Do example of reduction now
+    	 */
+    	reductionExample();
+    	
+    	/**
+    	 * Do example of groupby now
+    	 */
+    	grouping();
     }
     
     private static void peek(Integer i)
@@ -163,5 +247,124 @@ public class App
     	{
     		data[i] = transformer.transform(data[i]);
     	}
+    }
+    
+    /**
+     * Reduction usage
+     */
+    private static void reductionExample()
+    {
+    	System.out.println("-------");
+    	
+    	List<String> names = new ArrayList<String>();
+    	names.add("Abby");
+    	names.add("Becky");
+    	names.add("Claire");
+    	names.add("Dickhead");
+    	names.add("Emma");
+    	names.add("Fransisca");
+    	names.add("Gabriella");
+    	names.add("Jess");
+    	names.add("Jane");
+    	List<String> safeNames = Collections.synchronizedList(names);
+    	
+    	/**
+    	 * Let's create our parallel stream now
+    	 */
+    	Stream<String> namesStream = safeNames.parallelStream();
+    	
+    	/**
+    	 * Reduction allows one to perform an operation
+    	 * of a function taking in (left, right) and
+    	 * producing a result of the type of those two
+    	 * elements (a <code>String</code> in this case).
+    	 * 
+    	 * However, the reduction part is that the first
+    	 * argument to reduce is always `left` whilst the
+    	 * `right` is (per iteration) a stream element
+    	 */
+    	String reduction = namesStream.reduce("Hello", (lStr, rStr) -> (lStr+rStr));
+    	System.out.println(reduction);
+    }
+    
+    public record Person(String name)
+    {
+    	
+    }
+    
+    public record Receipt(Person person, int amount)
+    {
+    	
+    }
+    
+    /**
+     * Playing around with grouping
+     */
+    private static void grouping()
+    {
+    	System.out.println("-------");
+    	
+    	/**
+    	 * Let's make a list of some receipts
+    	 */
+    	List<Receipt> receipts = new ArrayList<Receipt>();
+    	receipts.add(new Receipt(new Person("Abby"), 100));
+    	receipts.add(new Receipt(new Person("Becky"), 100));
+    	receipts.add(new Receipt(new Person("Abby"), 60));
+    	receipts.add(new Receipt(new Person("Claire"), 50));
+    	receipts.add(new Receipt(new Person("Dickhead"), 100));
+    	receipts.add(new Receipt(new Person("Abby"), 10));
+    	receipts.add(new Receipt(new Person("Dickhead"), 100));
+    	receipts.add(new Receipt(new Person("Emma"), 100));
+    	receipts.add(new Receipt(new Person("Abby"), 1000));
+    	receipts.add(new Receipt(new Person("Abby"), 1));
+    	receipts.add(new Receipt(new Person("Dickhead"), 70));
+    	receipts.add(new Receipt(new Person("Jess"), 100));
+    	
+    	
+    	/**
+    	 * Let's create a stream of <code>Receipt</code>'s
+    	 */
+    	List<Receipt> safeReceipts = Collections.synchronizedList(receipts);
+    	Stream<Receipt> receiptStream = safeReceipts.parallelStream();
+    	
+    	/**
+    	 * We will now collect these using a <code>groupby()</code>
+    	 * <code>Collector</code>.
+    	 * 
+    	 * We will pass a function that will be passed the stream's
+    	 * current element and return some value of type T which
+    	 * will be used as the key in a map. Each time it matches
+    	 * a given key it will append to a list of Receipts of
+    	 * that key
+    	 * 
+    	 * For example, we will write a function which takes in a
+    	 * <code>Receipt</code> and then we pass it the method
+    	 * reference <code>Receipt::person</code> and it will
+    	 * match all Receipt's with that person to a list for
+    	 * receipts made by that person. This method reference
+    	 * returns the current stream element's person object
+    	 * and that will be placed as the key in a Map<T,List<Receipt>>
+    	 * and because the equality of records is based on the
+    	 * components of it (Person just has a name) it will find
+    	 * multiple instances with matching `name`'s and hence
+    	 * place those in the same bucket
+    	 * 
+    	 * There is a concurrent collector and non-concurrent,
+    	 * the concurrent one is good to parallelize but of
+    	 * course the underlying map implementation must be good.
+    	 * 
+    	 * But you can see how that affects the order of listed
+    	 * receipts as the value for a given key.
+    	 */
+    	Map<Person, List<Receipt>> receiptsGrouped = receiptStream.collect(Collectors.groupingByConcurrent(Receipt::person));
+    	
+    	for(Entry<Person, List<Receipt>> entry: receiptsGrouped.entrySet())
+    	{
+    		System.out.println("Purchases by '"+entry.getKey()+"': "+entry.getValue());
+    	}
+
+    	
+    	
     }
 }
